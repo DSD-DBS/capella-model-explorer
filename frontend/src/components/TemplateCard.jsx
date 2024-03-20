d2226e2dc06a4ad95b1446abaa3f89f99e7dda7d
freshavocado7 <nikidimitriou97@gmail.com>
2024-03-20 10:40:33 +0100

    style: Unisize boxes of same row in templates views

--- frontend/src/components/TemplateCard.jsx
+++ frontend/src/components/TemplateCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const TemplateCard = ({template, onClickCallback}) => (
     <div onClick={() => onClickCallback(template.idx)}
-        className='max-w-sm bg-white rounded-lg border border-gray-200 shadow-md m-4 cursor-pointer hover:bg-gray-100'>
+        className='max-w-sm bg-white rounded-lg border border-gray-200 shadow-md m-2 cursor-pointer hover:bg-gray-100'>
         <div className='p-5'>
             <h5 className='mb-2 text-2xl font-bold text-gray-900'>
                 {template.name}
