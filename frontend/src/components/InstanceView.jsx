38565f4a169f7121b661c9906ac01d2bea4561fe
vik378 <viktor.kravchenko@deutschebahn.com>
2024-04-24 15:15:13 +0200

    feat: doc scrolls

--- frontend/src/components/InstanceView.jsx
+++ frontend/src/components/InstanceView.jsx
@@ -67,7 +67,7 @@ export const InstanceView = ({ templateName, objectID, endpoint }) => {
   return (
     <div
       ref={contentRef}
-      className={`html-content rounded-lg border-4 border-transparent bg-gray-100 p-8 text-gray-700 shadow-lg scrollbar scrollbar-track-gray-200 scrollbar-thumb-gray-400 hover:border-gray-300 dark:bg-custom-dark-2 dark:text-gray-100 dark:shadow-dark dark:scrollbar-track-custom-dark-3 dark:scrollbar-thumb-slate-600 md:w-[210mm] print:m-0 print:bg-transparent print:p-0 print:shadow-none ${
+      className={`html-content rounded-lg border-4 border-transparent bg-white p-8 text-gray-700 shadow-lg scrollbar scrollbar-track-gray-200 scrollbar-thumb-gray-400 hover:border-gray-300 dark:bg-custom-dark-2 dark:text-gray-100 dark:shadow-dark dark:scrollbar-track-custom-dark-3 dark:scrollbar-thumb-slate-600 md:w-[210mm] print:m-0 print:bg-transparent print:p-0 print:shadow-none ${
         isHovering ? "z-50 shadow-md" : ""
       }`}
       onMouseEnter={() => setIsHovering(true)}
