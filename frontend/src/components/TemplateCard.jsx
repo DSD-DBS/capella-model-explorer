513b56d21417e64030dacfb8fc3299703675fd23
vik378 <viktor.kravchenko@deutschebahn.com>
2024-04-25 13:06:18 +0200

    feat: improved default templates

--- frontend/src/components/TemplateCard.jsx
+++ frontend/src/components/TemplateCard.jsx
@@ -9,7 +9,7 @@ export const TemplateCard = ({ template, onClickCallback }) => (
     className="m-2 mt-6 max-w-sm cursor-pointer rounded-lg bg-gray-200 shadow-md hover:bg-custom-light dark:bg-custom-dark-2 dark:shadow-dark dark:hover:bg-custom-dark-4"
   >
     <div className="p-5">
-      <h5 className="mb-2 text-2xl font-bold text-gray-900 dark:text-gray-100">
+      <h5 className="mb-2 text-2xl font-normal text-gray-900 dark:text-gray-100">
         {template.name}
       </h5>
       <p className="mb-3 font-normal text-gray-700 dark:text-gray-300">
